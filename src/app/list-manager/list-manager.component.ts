f2fd0a546f0bdb184795d118d38d2d98613edc9f
Anita Żuchowska <anitazuchowska@biuro.k2.pl>
2018-11-17 16:26:04 +0100

    add item using service

---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+import { TodoListService } from '../services/todo-list.service';
+​
+@Component({
+    selector: 'app-list-manager',
+    templateUrl: './list-manager.component.html',
+    styleUrls: ['./list-manager.component.css']
+})
+export class ListManagerComponent implements OnInit {
+    todoList: TodoItem[];
+
+    constructor(private todoListService: TodoListService) { }
+
+    ngOnInit() {
+        this.todoList = this.todoListService.getTodoList();
+    }
+
+    addItem(title: string) {
+        this.todoListService.addItem({ title });
+    }
+}
